92a898043e92aee55198236bc062207b4457b005
axelkelman <akelman@fi.uba.ar>
2020-07-24 02:48:49 -0300

    version final

--- deps.mk
+++ deps.mk
@@ -1,4 +1,1 @@
-zyxcba: zyxcba.o strutil.o mensajes.o funciones_tp2.o lista.o
-funciones_tp2: funciones_tp2.o hash.o heap.o abb.o lista.o 
-hash: hash.o lista.o pstdint.o
-abb: abb.o lista.o pila.o
+zyxcba: zyxcba.o strutil.o funciones_tp2.o hash.o abb.o heap.o pila.o cola.o lista.o
